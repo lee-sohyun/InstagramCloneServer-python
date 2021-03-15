b40646d86be69f10e579c896085475f44609fdf8
이소현 <sohlee77@gmail.com>
2021-03-15 06:14:56 +0000

    feat(account): setting forms

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,48 @@
 from django import forms
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+from .models import Profile
+
 
 class SignupForm(UserCreationForm):
-    pass
+    user = form.CharField(label='사용자명', widget=forms.TextInput(attrs={
+        'pattern': '[a-zA-z0-9]+',
+        'title': '특수문자, 공백 입력불가',
+        }))
+    
+    nickname = forms.CharField(label='닉네임')
+    picture = forms.ImageField(label='프로필 사진', required=False)
+    
+    class Meta(UserCreationForm.Meta):
+        fields = UserCreationForm.Meta.fields + ('email',)
+        
+    def clean_nickname(self):
+        nickname = self.cleaned_data.get('nickname')
+        if Profile.objects.filter(nickname=nickname).exists():
+            raise forms.ValidationError('이미 존재하는 닉네임 입니다')
+        return nickname
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        User = get_user_model()
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError('사용중인 이메일 입니다')
+        return email
+    
+    def clean_picutre(self):
+        picture = self.cleaned_data.get('picture')
+        if not picutre:
+            picture = None
+        return picture
+    
+    def save(self):
+        user = super().save()
+        Profile.objects.create(
+            user=user,
+            nickname=self.cleaned_data['nickname'],
+            picture=self.cleaned_data['picture'],
+        )
+        return user
+    
