8c22fd00bf212753f45e1a0d42c07ef3cfc46ca8
이소현 <sohlee77@gmail.com>
2021-03-18 07:57:46 +0000

    fix(post): check spell

--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,20 @@
+from django import forms
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+class PostForm(forms.ModelForm):
+    content = forms.CharField(widget=forms.Textarea)
+    
+    class Meta:
+        model = Post
+        fields = '__all__'
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display=['id', 'author', 'nickname', 'content', 'created_at']
+    list_display_links = ['author', 'nickname', 'content']
+
+    
+    def nickname(request, post):
+        return post.author.profile.nickname
+    
